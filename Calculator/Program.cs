9cc40a91152184817928fdc78aaa9ee08a7b4c5e
Anisa Hassan <au592172@post.au.dk>
2019-02-05 13:40:46 +0100

    Added test i main

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-           
+           calculator calc = new calculator();
 
+            Console.WriteLine(calc.Add(5,1));
+            Console.WriteLine(calc.Subtract(5, 4));
+            Console.WriteLine(calc.Multiply(5, 2));
+            Console.WriteLine(calc.Power(6, 1));
+            Console.WriteLine(calc.Add(5, 10));
+            Console.WriteLine(calc.Add(12, 1));
         }
     }
 }
